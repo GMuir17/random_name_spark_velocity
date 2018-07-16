402e00ba759c662eb6fa8abb32e7b28f5b26d1fa
Gary Muir <garymuir17@gmail.com>
2018-07-16 15:35:11 +0100

    initial commit

---
+++ src/main/java/RandomName.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class RandomName {
+
+    private ArrayList<String> names;
+
+    public RandomName() {
+        names.add("Gaius");
+        names.add("Crassus");
+        names.add("Brutus");
+        names.add("Julius");
+        names.add("Pompianus");
+        names.add("Marcus");
+    }
+
+
+
+}
